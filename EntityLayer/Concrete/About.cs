49d8191e428d145f19b26faa5fc68f6ed292256e
Muhammed Can GEZGİN <93928479+Cangezgin1@users.noreply.github.com>
2023-02-21 14:17:34 +0300

    Update About.cs

--- EntityLayer/Concrete/About.cs
+++ EntityLayer/Concrete/About.cs
@@ -11,12 +11,19 @@ namespace EntityLayer.Concrete
     {
         [Key]
         public int AboutID { get; set; }
+        [StringLenght(10)]
         public string Title { get; set; }
+        [StringLenght(100)]
         public string Description { get; set; }
+        [StringLenght(2)]
         public string Age { get; set; }
+        [StringLenght(25)]
         public string Mail { get; set; }
+        [StringLenght(11)]
         public string Phone { get; set; }
+        [StringLenght(100)]
         public string Address { get; set; }
+        [StringLenght(250)]
         public string ImageUrl { get; set; }
     }
 }
